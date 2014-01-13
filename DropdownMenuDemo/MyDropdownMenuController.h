d5519b30b2dd79269a4a10e80e1d22cf45c93c2b
Nils Mattisson <nils.mattisson@gmail.com>
2014-01-13 13:45:20 +0100

    Adding SubClass for DropdownMenuController. Moving Readme, podspec and License to outside core files.

---
+++ DropdownMenuDemo/MyDropdownMenuController.h
@@ -0,0 +1,13 @@
+//
+//  MyDropdownMenuController.h
+//  DropdownMenuDemo
+//
+//  Created by Nils Mattisson on 1/13/14.
+//  Copyright (c) 2014 Nils Mattisson. All rights reserved.
+//
+
+#import "DropdownMenuController.h"
+
+@interface MyDropdownMenuController : DropdownMenuController
+
+@end
